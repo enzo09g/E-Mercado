f74b7e4b86333a8fd2b0778f4f232bbd66bd5c53
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2023-08-19 16:09:27 -0300

    arreglar órden y comentarios

--- js/products.js
+++ js/products.js
@@ -1,14 +1,14 @@
-// Guardamos en una constante el endpoint que contiene la info de los productos de categoría autos
+// Guardamos en una constante el endpoint que contiene la info de los productos de categoría productos
 
 const apiCars =
   "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
 // Guardamos en una constante el elemento con id lista-productos
-// A este elemento le vamos a agregar los elementos con la información de cada auto como hijo
+// A este elemento le vamos a agregar los elementos con la información de cada producto como hijo
 
 const contenedorLista = document.getElementById("lista-productos");
 
-// hacemos fetch al endpoint con la información de los autos y los adjunta al elemento contenedorLista
+// hacemos fetch al endpoint con la información de los productos y los adjunta al elemento contenedorLista
 
 function getProducts() {
   fetch(apiCars)
